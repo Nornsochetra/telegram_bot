dcbffebc08174042c17c242583dbac6e28dda495
Nornsochetra <nornsochetra@gmail.com>
2026-01-21 22:51:17 +0700

    finished send message notification

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+from time import sleep
+
+token = "8530640577:AAF1ZFTaeBTG3bxdTb9jLobJuCuH2kAflGM"
+
+# testing token is it work or not
+# url = f"https://api.telegram.org/bot{token}/getMe"
+#
+# headers = {
+#     "accept": "application/json",
+#     "User-Agent": "Telegram Bot SDK - (https://github.com/irazasyed/telegram-bot-sdk)"
+# }
+#
+# response = requests.post(url, headers=headers)
+#
+# print(response.text)
+
+# send notification
+url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+payload = {
+    "text": "hello world my name is chetra", # text we want to see
+    "parse_mode": "HTML", # format text
+    "disable_web_page_preview": False,
+    "disable_notification": False,
+    "chat_id": "@su25_notification_testing" # username group
+}
+headers = {
+    "accept": "application/json",
+    "User-Agent": "so chetraa",
+    "content-type": "application/json"
+}
+
+for response in range(100):
+    response = requests.post(url, json=payload, headers=headers)
+    print(response.text)
+    sleep(3)
